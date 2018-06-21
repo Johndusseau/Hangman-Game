354dcf8632fd5b7d81b652cf983a58685e475795
john dusseau <johndusseau92@gmail.com>
2018-06-21 16:40:53 -0500

    debugged

---
+++ wordbank.js
@@ -0,0 +1,16 @@
+Words = {};
+Words.List = [];
+
+Words.List[0] = "dog";
+Words.List[1] = "goat";
+Words.List[2] = "snake";
+Words.List[3] = "horse";
+Words.List[4] = "sheep";
+Words.List[5] = "turttle";
+Words.List[6] = "fish";
+Words.List[7] = "wolf";
+Words.List[8] = "bear";
+Words.List[9] = "penguin";
+Words.List[10] = "shark";
+
+Words.Length = Words.List.length;
